d882d41ffaf6f1f7e3a640f921ce86b66689b459
Jay Collett <486430+jaycollett@users.noreply.github.com>
2019-11-13 13:51:09 -0500

    initial commit

---
+++ MonoDockerTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace MonoDockerTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm alive...");
+
+            while (true)
+            {
+                Console.WriteLine($"It's currently {DateTime.Now}, and I'm still alive...");
+                Thread.Sleep(60000);
+            }
+        }
+    }
+}
